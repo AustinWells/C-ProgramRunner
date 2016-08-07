faff280d6e5c2c9ab8b06db7c771e88159575844
Arjun Dhiman <arjun.dhiman@mail.utoronto.ca>
2016-08-07 01:58:39 -0400

    Update crunner.sh

--- crunner.sh
+++ crunner.sh
@@ -3,15 +3,14 @@ echo "Welcome to the C program runner"
 echo "        By Arjun Dhiman        "
 echo "_______________________________"
 
-echo "Please enter in the file name followed that you would like to run." 
-read name
-while(expr length $name > 0)
-do
-echo "Program out:"
-echo "____________"
-gcc "$name.c"
-./a.out
-echo " \n "
-echo " Please enter in the file name followed that you would like to run"
-read name
-done
+if test $# -ne 1
+  then
+     echo "Please supplie a .c file to compile"
+fi
+
+for var in "$@"
+ do
+    echo "________ Compiling $var _________"
+    echo gcc -Wall -o "$var" "$var".c
+    gcc -Wall -o "$var" "$var".c
+ done
